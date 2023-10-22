3ccaf5cb24cdfd35936e73354b87fc5213419ab4
Vladislav Kryuchkovsky <evakru2009@gmail.com>
2023-10-22 04:02:58 +0300

    Added ecs bootstrapper

---
+++ Assets/_GameLogic/Core/LogicSceneEcsBootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using _GameLogic.Core.GameStates.Systems;
+using Scellecs.Morpeh;
+
+namespace _GameLogic.Core
+{
+    public class LogicSceneEcsBootstrapper : EcsBootstrapper
+    {
+        public override World World => World.Default;
+
+        protected override void RegisterSystems()
+        {
+            AddInitializer<GameStateMachineInitializingSystem>()
+                .AddSystem<GalaxyLoadingSystem>()
+                .AddSystem<MainMenuLoadingSystem>()
+                .AddSystem<PlayStateSwitchingOnRequestProcessingSystem>();
+        }
+    }
+}
